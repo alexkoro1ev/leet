2b928ae46ae54c2af26cabcb8ebc1ae2641bd06d
a1exkorolev <greatstaxan@gmail.com>
2022-08-02 22:54:57 +0500

    Added next solutions: 205. Isomorphic strings. That wasn't easy -_-

--- src/easy/isomorphic_strings/Solution.java
+++ src/easy/isomorphic_strings/Solution.java
@@ -1,7 +1,26 @@
 package easy.isomorphic_strings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
   public boolean isIsomorphic(String s, String t) {
-      return true;
+      return modifying(s).equals(modifying(t));
   }
+
+    public String modifying(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), i);
+            }
+
+            builder.append(map.get(s.charAt(i)));
+            builder.append(" ");
+        }
+
+        return builder.toString();
+    }
 }
